6bf006e1aa09a8e481a4795f2533fb48aca5eb42
AkakiGoginava <akakigoginava2808@gmail.com>
2025-05-18 00:11:02 +0400

    refactor: apply requested changes

--- resources/views/components/forms/form.blade.php
+++ resources/views/components/forms/form.blade.php
@@ -1,7 +1,7 @@
-<form action="/login" method="POST" class="w-[462px] flex-1 flex flex-col justify-center gap-7">
+<form action="/login" method="POST" class="w-[28.875rem] flex-1 flex flex-col justify-center gap-7">
     @csrf
 
-    <div class="text-[32px] font-bold">
+    <div class="text-[2rem] font-bold">
         {{ $header ?? '' }}
     </div>
 
