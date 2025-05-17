3cc5864298d3c065a0d6abbe82c77c2d5a32045c
AkakiGoginava <akakigoginava2808@gmail.com>
2025-05-17 20:02:38 +0400

    refactor: extract input field into reusable component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,53 +25,14 @@
                     <img src="{{ asset('svg/smile.svg') }}" alt="Smile icon" class="w-15 h-12 mt-2">
                 </div>
 
-                <div>
-                    <div
-                        class="w-full h-17 px-3 py-2 relative border border-white rounded-xl
-                            content-center focus-within:border-blue-600 peer-invalid:border-red-500">
+                <x-forms.input-field type="email" name="email" placeholder="Write your e-mail" required>
+                    E-mail
+                </x-forms.input-field>
 
-                        <input type="email" required placeholder="Write your e-mail"
-                            class="peer absolute top-6 w-full bg-transparent 
-                            text-sm px-3 py-2 transition duration-300
-                            placeholder-transparent text-gray-800
-                            focus:outline-none focus:placeholder-slate-400" />
-
-                        <label
-                            class="absolute left-6 top-3 text-xs text-gray-800
-                            transition-all transform origin-left
-                            pointer-events-none
-                            peer-placeholder-shown:top-5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-600
-                            peer-focus:top-3 peer-focus:text-xs peer-focus:text-gray-800">
-                            E-mail
-                        </label>
-                    </div>
-
-                    <label for="email" class="pl-6 mt-1 text-xs text-red-500">Invalid email</label>
-                </div>
-
-                <div>
-                    <div
-                        class="w-full h-17 px-3 py-2 relative border border-white rounded-xl
-                            content-center focus-within:border-blue-600 peer-invalid:border-red-500">
-
-                        <input type="password" required placeholder="Write your password"
-                            class="peer absolute top-6 w-full bg-transparent 
-                            text-sm px-3 py-2 transition duration-300
-                            placeholder-transparent text-gray-800
-                            focus:outline-none focus:placeholder-slate-400" />
-
-                        <label
-                            class="absolute left-6 top-3 text-xs text-gray-800
-                            transition-all transform origin-left
-                            pointer-events-none
-                            peer-placeholder-shown:top-5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-600
-                            peer-focus:top-3 peer-focus:text-xs peer-focus:text-gray-800">
-                            Password
-                        </label>
-                    </div>
-
-                    <label for="email" class="pl-6 mt-1 text-xs text-red-500">Invalid password</label>
-                </div>
+                <x-forms.input-field type="password" name="password" placeholder="Write your password" minlength='4'
+                    required>
+                    Password
+                </x-forms.input-field>
 
                 <button type="submit"
                     class="w-full h-17 bg-blue-400 text-white font-bold rounded-xl transition 
---
+++ resources/views/components/forms/input-field.blade.php
@@ -0,0 +1,27 @@
+@props(['name'])
+
+<div>
+    <div
+        class="w-full h-17 px-3 py-2 relative border border-white rounded-xl
+            content-center focus-within:border-blue-600 peer-invalid:border-red-500">
+
+        <input {{ $attributes->merge() }}
+            class="peer absolute top-6 w-full bg-transparent 
+                text-sm px-3 py-2 transition duration-300
+                placeholder-transparent text-gray-800
+                focus:outline-none focus:placeholder-slate-400" />
+
+        <label
+            class="absolute left-6 top-3 text-xs text-gray-800
+                transition-all transform origin-left
+                pointer-events-none
+                peer-placeholder-shown:top-5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-600
+                peer-focus:top-3 peer-focus:text-xs peer-focus:text-gray-800">
+            {{ $slot }}
+        </label>
+    </div>
+
+    @error($name)
+        <p class="pl-6 mt-1 text-xs text-red-500">{{ $message }}</p>
+    @enderror
+</div>
