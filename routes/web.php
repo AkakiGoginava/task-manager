9f92c1b7f36e012f80fd7db90a062c49c7c6e32b
AkakiGoginava <akakigoginava2808@gmail.com>
2025-05-18 16:30:13 +0400

    chore: add middleware to tasks route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'auth.login');
-Route::view('/tasks', 'tasks.index');
+Route::view('/tasks', 'tasks.index')->middleware('auth');
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
