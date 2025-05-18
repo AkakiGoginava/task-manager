3259ad4365d6fa22ab997ff1c9ff33faf79a6870
AkakiGoginava <akakigoginava2808@gmail.com>
2025-05-18 12:28:00 +0400

    feat: add register user command

---
+++ app/Console/Commands/RegisterUser.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterUser extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'app:register-user';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Register user';
+
+	/**
+	 * Execute the console command.
+	 */
+	public function handle()
+	{
+		// Validate all inputs immediately
+
+		do {
+			$name = $this->ask('Enter your name');
+
+			$validator = Validator::make(
+				['name' => $name],
+				['name' => 'required']
+			);
+
+			if ($validator->fails()) {
+				$this->error($validator->errors()->first('email'));
+			}
+		} while ($validator->fails());
+
+		do {
+			$email = $this->ask('Enter your email');
+
+			$validator = Validator::make(
+				['email' => $email],
+				['email' => 'required|email|unique:users']
+			);
+
+			if ($validator->fails()) {
+				$this->error($validator->errors()->first('email'));
+			}
+		} while ($validator->fails());
+
+		do {
+			$password = $this->secret('Enter your password');
+
+			$validator = Validator::make(
+				['password' => $password],
+				['password' => 'required|min:4']
+			);
+
+			if ($validator->fails()) {
+				$this->error($validator->errors()->first('password'));
+			}
+		} while ($validator->fails());
+
+		do {
+			$passwordConfirmation = $this->secret('Confirm password');
+
+			if ($password !== $passwordConfirmation) {
+				$this->error('Passwords do not match, try again');
+			}
+		} while ($password !== $passwordConfirmation);
+
+		User::create([
+			'name'     => $name,
+			'email'    => $email,
+			'password' => Hash::make($password),
+		]);
+
+		$this->info('User created successfully');
+	}
+}
