1d641ecb292da10db64fb912f7713b95d4e55215
AkakiGoginava <akakigoginava2808@gmail.com>
2025-05-18 14:10:25 +0400

    refactor: apply requested changes

--- app/Console/Commands/RegisterUser.php
+++ app/Console/Commands/RegisterUser.php
@@ -4,32 +4,16 @@ namespace App\Console\Commands;
 
 use App\Models\User;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterUser extends Command
 {
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
 	protected $signature = 'app:register-user';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
 	protected $description = 'Register user';
 
-	/**
-	 * Execute the console command.
-	 */
 	public function handle()
 	{
-		// Validate all inputs immediately
-
 		do {
 			$name = $this->ask('Enter your name');
 
@@ -80,7 +64,7 @@ class RegisterUser extends Command
 		User::create([
 			'name'     => $name,
 			'email'    => $email,
-			'password' => Hash::make($password),
+			'password' => $password,
 		]);
 
 		$this->info('User created successfully');
