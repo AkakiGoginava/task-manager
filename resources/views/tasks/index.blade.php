7ee461f525e4b058f4dd4a9a636153ed59bc8bd8
AkakiGoginava <akakigoginava2808@gmail.com>
2025-05-19 15:03:04 +0400

    feat(tasks page): add sidebar and footer

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,3 +1,55 @@
-<x-forms.form action="{{ route('logout') }}">
-    <x-forms.submit-btn>log out</x-forms.submit-btn>
-</x-forms.form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Your Tasks</title>
+</head>
+
+<body class="h-screen w-screen p-10">
+    <nav class="flex flex-col items-center h-full w-42 py-4 pl-5">
+        <img class="w-16 h-16 rounded-4xl mb-32" src="https://placehold.co/64x64" alt="profile picture">
+
+        <ul class="flex flex-col h-full gap-7 text-lg">
+            <li>
+                <a class="flex items-center gap-2 transition duration-300 hover:text-neutral-500"
+                    href="{{ route('tasks.index') }}">
+                    <img src="{{ asset('svg/tasksIcon.svg') }}" alt="tasks icon">
+                    My tasks
+                </a>
+            </li>
+            <li>
+                <a class="flex items-center gap-2 transition duration-300 hover:text-neutral-500" href="">
+                    <img src="{{ asset('svg/hourglass.svg') }}" alt="hourglass icon">
+                    Due tasks
+                </a>
+            </li>
+            <li>
+                <a class="flex items-center gap-2 transition duration-300 hover:text-neutral-500" href="">
+                    <img src="{{ asset('svg/profileIcon.svg') }}" alt="profile icon">
+                    Profile
+                </a>
+            </li>
+            <li class="mt-auto">
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button
+                        class="flex items-center gap-2 transition duration-300 hover:text-neutral-500 hover:cursor-pointer">
+                        <img src="{{ asset('svg/logoutIcon.svg') }}" alt="logout icon">
+                        Log out
+                    </button>
+                </form>
+            </li>
+        </ul>
+    </nav>
+
+    <footer class="flex justify-center gap-4 absolute bottom-10 right-10">
+        <p>English</p>
+        <p>ქართული</p>
+    </footer>
+</body>
+
+</html>
